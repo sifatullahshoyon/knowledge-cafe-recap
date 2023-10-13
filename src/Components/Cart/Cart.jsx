30e083018f0cdf229ec4e8afc15ecf99695407d7
freelancersifat627e <freelancersifat627@gmail.comm>
2023-10-14 02:57:46 +0600

    Added to the bookmark in the blog title

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const Cart = (props) => {
-    console.log(props)
-    return (
-        <div>
-            <div className='text-center bg-indigo-100 mb-6 mx-auto'>
-                <p className='border rounded border-indigo-500 p-5 font-exo text-2xl text-indigo-600 font-bold'>Spent time on read : 177 min</p>
-            </div>
-            <div className='bg-slate-100 p-7 rounded'>
-                <h1 className='text-2xl text-black font-exo font-bold'>Bookmarked Blogs : {props.cart.length}</h1> 
-            </div>
-        </div>
-    );
+  return (
+    <>
+      <div className="text-center bg-indigo-100 mb-6 mx-auto">
+        <p className="border rounded border-indigo-500 p-5 font-exo text-2xl text-indigo-600 font-bold">
+          Spent time on read : 177 min
+        </p>
+      </div>
+      <div className="bg-slate-100 p-7 rounded">
+        <h1 className="text-2xl text-black font-exo font-bold">
+          Bookmarked Blogs : {props.cart.length}
+        </h1>
+        {props.cart?.map((product) => (
+          <div key={product.id} className="my-4 bg-white p-5 rounded">
+            <p className="font-exo font-semibold text-lg leading-7">
+              {product.title}
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Cart;
