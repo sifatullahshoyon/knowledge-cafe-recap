918c876e934a6f89fae06e69a015ffaab11f2a14
freelancersifat627e <freelancersifat627@gmail.comm>
2023-10-14 00:40:46 +0600

    Primary Set Up Done For Starting Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-500 font-bold text-5xl'>Hello</h1>
+      <button className='btn btn-primary font-bold'>click me</button>
+    </div>
+  );
+};
+
+export default App;
