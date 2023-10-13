972e185816e3bd670e886fecc382899fbc137ab8
freelancersifat627e <freelancersifat627@gmail.comm>
2023-10-14 01:32:11 +0600

    Compleat Side bar design with fully responsive

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Cart = () => {
     return (
         <div>
-            
+            <div className='text-center bg-indigo-100 mb-6 mx-auto'>
+                <p className='border rounded border-indigo-500 p-5 font-exo text-2xl text-indigo-600 font-bold'>Spent time on read : 177 min</p>
+            </div>
+            <div className='bg-slate-100 p-7 rounded'>
+                <h1 className='text-2xl text-black font-exo font-bold'>Bookmarked Blogs : 8</h1> 
+            </div>
         </div>
     );
 };
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,46 +1,56 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 const Shop = (props) => {
-    console.log(props)
-    const {title, image, profile, beginners, programming, Mark_as_read} = props.product;
-    return (
-        <div>
-        <div className="card-compact w-full bg-base-100">
-          <figure>
-            <img className="rounded" src={image} alt="programming img" />
-          </figure>
-          <div className="card-body">
-            <div className="flex justify-between items-center my-8">
-              <div className="flex justify-center items-center gap-6">
-                <div>
-                  <img src={profile.img} alt="" />
-                </div>
-                <div>
-                  <h4 className="font-exo text-2xl text-black font-bold">{profile.name}</h4>
-                  <p>
-                    <small className="font-exo font-semibold text-base">{profile.published_in}</small>
-                  </p>
-                </div>
+  console.log(props);
+  const { title, image, profile, beginners, programming, Mark_as_read } = props.product;
+  return (
+    <div>
+      <div className="card-compact w-full bg-base-100">
+        <figure>
+          <img className="rounded" src={image} alt="programming img" />
+        </figure>
+        <div className="card-body">
+          <div className="flex justify-between items-center my-8">
+            <div className="flex justify-center items-center gap-6">
+              <div>
+                <img src={profile.img} alt="" />
               </div>
               <div>
+                <h4 className="font-exo text-2xl text-black font-bold">
+                  {profile.name}
+                </h4>
                 <p>
-                  <small className="font-exo font-medium text-xl">{profile.read} min read <FontAwesomeIcon icon={faBookmark} /></small>
+                  <small className="font-exo font-semibold text-base">
+                    {profile.published_in}
+                  </small>
                 </p>
               </div>
             </div>
-            <h2 className="card-title mb-4 font-exo font-bold text-4xl text-black leading-[64px]">{title}</h2>
-            <div className="flex gap-2 mb-5 text-xl font-medium">
-              <span>{beginners}</span>
-              <span>{programming}</span>
+            <div>
+              <p>
+                <small className="font-exo font-medium text-xl">
+                  {profile.read} min read <FontAwesomeIcon icon={faBookmark} />
+                </small>
+              </p>
             </div>
-            <p className="font-exo text-xl font-semibold text-indigo-600 underline">{Mark_as_read}</p>
-            <hr className="my-8" />
           </div>
+          <h2 className="card-title mb-4 font-exo font-bold text-4xl text-black leading-[64px]">
+            {title}
+          </h2>
+          <div className="flex gap-2 mb-5 text-xl font-medium">
+            <span>{beginners}</span>
+            <span>{programming}</span>
+          </div>
+          <p className="font-exo text-xl font-semibold text-indigo-600 underline">
+            {Mark_as_read}
+          </p>
+          <hr className="my-8" />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Shop;
