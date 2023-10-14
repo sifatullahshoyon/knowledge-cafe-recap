5458f5d841c8c312d6465fb6cfb00f3787a2a6d6
freelancersifat627e <freelancersifat627@gmail.comm>
2023-10-14 20:54:31 +0600

    Added Faq question & answer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
+import Blog from './Components/Blog/Blog';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+
 
 const App = () => {
   return (
     <>
       <Header></Header>
       <Main></Main>
+      <Blog></Blog>
       <ToastContainer />
     </>
   );
